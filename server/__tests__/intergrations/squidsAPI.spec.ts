34cce2bcaa6d7505960c6e5b4651d858d6db3217
nadermokhtar <nader.mokhtar@gmail.com>
2022-11-06 21:09:10 -0500

    fixed broken syntax in test

--- server/__tests__/intergrations/squidsAPI.spec.ts
+++ server/__tests__/intergrations/squidsAPI.spec.ts
@@ -2,9 +2,8 @@ import request from "supertest";
 import app from "../../src/app";
 
 describe("GET /squids", function () {
-  it("responds with json", async () {
-    const res = await request(app)
-      .get("/squids")
+  it("responds with json", async () => {
+    const res = await request(app).get("/squids");
 
     expect(res.headers["Content-Type"]).toMatch(/json/);
     expect(res.status).toEqual(200);
